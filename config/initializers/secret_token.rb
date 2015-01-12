1f590166ed189e26edba7f3ec025f082d1b6b367
tyler liu <replicat0r.misc@gmail.com>
2015-01-12 15:35:54 -0500

    first commit, mysql db setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SimpleCms::Application.config.secret_key_base = 'cae41687d4b02d8b765321298a1f673825cf7422b2cf857b1de1ed3f6015a1568c814e43e63dd2c6b117ebffd9e8cc5834171e76d1fe3543c28b12958783eb55'
